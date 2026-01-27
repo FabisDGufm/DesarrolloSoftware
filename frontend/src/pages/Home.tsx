8afa8a4c57353b4cfab0d73f13764ee1d9e58db6
FabisDGufm <fabioladerasg@ufm.edu>
2026-01-27 06:51:26 -0600

    Hello world - front end

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,7 @@
-import styles from './Home.module.css'
-
 export function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>El Pasillo</h1>
-      <p className={styles.subtitle}>Red social universitaria de Guatemala</p>
+    <div>
+      <h1>Hello World</h1>
     </div>
   )
 }
