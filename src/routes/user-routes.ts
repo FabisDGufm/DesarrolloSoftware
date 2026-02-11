f5556afd2d4564a88d5690743cfdac3dbc4bf589
FabisDGufm <fabioladerasg@ufm.edu>
2026-02-10 21:11:47 -0600

    User's CRUD

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -7,6 +7,22 @@ const router = Router();
 const userService = new UserService();
 const userController = new UserController(userService);
 
-router.post('/register', userController.register);
+// Create user
+router.post('/register', userController.registerUser);
+
+// Read all
+router.get('/', userController.getUsers);
+router.get('/:name', userController.getUserbN);
+
+// Uptate
+router.put('/:id/name', userController.updateUserN)
+router.put('/:id/email', userController.updateUserEmail);
+router.put('/:id/password', userController.updateUserP)
+
+//Delete
+router.delete('/:id', userController.deleteUser);
+
+// Get friends
+router.get('/:id/friends', userController.getFriends);
 
 export default router;
--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,4 +1,4 @@
-import type { User, CreateUserDTO } from '../models/user.js';
+import type { User, CreateUserDTO, UpdateUserDTO } from '../models/user.js';
 
 export class UserService {
     private passwordMinLength: number = 8;
@@ -22,8 +22,53 @@ export class UserService {
             friends: [],
             createdAt: new Date()
         };
-
+        
         console.log("User saved to DB:", newUser);
         return newUser;
+    }
+
+    // Read all
+    getAllUsers(): User[] {
+        console.log("Getting all users from DB");
+        return [];
+    }
+
+    // Read user by name
+    getUserbN(name: string): User | undefined {
+        console.log("Getting user: ", name);
+        return undefined;
+    }
+
+    // Update user name
+    updateUserN(id: number, name: string): boolean {
+        console.log("Updating user's old name to:", name);
+        return true;
+    }
+
+    // Update user email
+    updateUserEmail(id: number, email: string): boolean {
+        console.log("Updating user's email to ", email);
+        return true;
+    }
+
+    // Update user password
+    updateUserP(id: number, password: string): boolean {
+        console.log("Updating user's password");
+        if(password.length < this.passwordMinLength) {
+            throw new Error("Password must be at least 8 characters");
+        }
+        return true
+    }
+
+    //Delete
+    deleteUser(id: number): boolean{
+        console.log("Deleting user")
+        return true;
+    }
+
+    // Obtener amigos
+    getFriends(id: number): number[] {
+        console.log("Getting friends to user...");
+        return [3,4,8];
     }
 }
