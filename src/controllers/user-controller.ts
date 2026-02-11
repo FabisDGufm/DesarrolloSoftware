570683500bf9e6facfe0a34083bfbec018768760
FabisDGufm <fabioladerasg@ufm.edu>
2026-02-10 21:11:47 -0600

    User's CRUD

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,18 +1,68 @@
 import type { Request, Response } from 'express';
-import { UserService } from '../services/user-service.js';
+import type { UserService } from '../services/user-service.js';
+import type { User } from '../models/user.js'
 
 
 export class UserController {
-    constructor(private userService: UserService) {
+    constructor(private readonly userService: UserService) {
         console.log("UserController initialized");
     }
 
-    register = (req: Request, res: Response) => {
-        try {
-            const user = this.userService.registerUser(req.body);
-            res.status(201).json(user);
-        } catch (error: any) {
-            res.status(400).json({ error: error.message });
-        }
+    // Create
+    registerUser = (_req: Request, _res: Response) => {
+        let user : User = _req.body;
+        let registerUser = this.userService.registerUser(user);
+
+        _res.json(registerUser);
+    }
+
+    // Read all
+    getUsers = (_req: Request, _res: Response) => {
+        let users = this.userService.getAllUsers();
+        _res.json(users);
+    }
+
+    // Read by name
+    getUserbN = (_req: Request, _res: Response) => {
+        let name = String(_req.params.name);
+        let user = this.userService.getUserbN(name);
+        _res.json(user);
+    }
+
+    // Update user name
+    updateUserN = (_req: Request, _res: Response) => {
+        let id = Number(_req.params.id);
+        let { user } = _req.body;
+        let result = this.userService.updateUserN(id, user);
+        _res.json(result);
+    }
+
+    // Update user email
+    updateUserEmail = (_req: Request, _res: Response) => {
+        let id = Number(_req.params.id);
+        let { email } = _req.body;
+        let result = this.userService.updateUserEmail(id, email);
+        _res.json(result);
+    }
+
+    // Update user password
+    updateUserP = (_req: Request, _res: Response) => {
+        let id = Number(_req.params.id);
+        let { password } = _req.body;
+        let result = this.userService.updateUserP(id, password);
+        _res.json(result);
+    }
+
+    // Delete
+    deleteUser = (_req: Request, _res: Response) => {
+        let id = Number(_req.params.id);
+        let result = this.userService.deleteUser(id);
+        _res.json(result);
+    }
+
+    getFriends = (_req: Request, _res: Response) => {
+        let id = Number(_req.params.id);
+        let friends = this.userService.getFriends(id);
+        _res.json(friends);
     }
 }
