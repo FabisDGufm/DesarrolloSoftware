de4e676d8a8006335af80911a1f8bdf0f1583c75
Mati05-1 <matiasarroyo@ufm.edu>
2026-03-17 06:25:43 -0600

    Esqueleto Explore (search): simulacion + unit e integration tests

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,9 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
-  testMatch: ['**/tests/**/*.test.ts'],  // ← Cambiar de __tests__ a tests
+  testMatch: ['**/tests/**/*.test.ts'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
-  extensionsToTreatAsEsm: ['.ts'],
-  transform: {
-    '^.+\\.ts$': ['ts-jest', {
-      useESM: true,
-    }],
-  },
+  clearMocks: true,
 };
